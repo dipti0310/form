19555c1ae6aa7a12494fecb7ed0491e15bc15070
Dipti Patel <diptipatel3oct2001@gmail.com>
2023-07-01 23:55:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbService= require('./dbservice')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// create 
+app.post('/insert',(request,response)=>{
+    console.log(request.body);
+    console.log("HERE");
+    const {FName,LName,Dob,Parent_name,Address,city,phone} = request.body;
+    const db = dbService.getDbServiceInstance();
+    
+    const result = db.insertNewName(FName,LName,Dob,Parent_name,Address,city,phone);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
+
+})
+
+
+// read 
+app.get('/getAll',(request,response)=>{
+    console.log('test');
+    const db=dbService.getDbServiceInstance();
+    const result= db.getAllData();
+    console.log("-----"+result);
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+
+app.listen(3000,()=>console.log('app is running'));
---
+++ dbservice.js
@@ -0,0 +1,95 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance=null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('db ' + connection.state);
+});
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM student_details;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            console.log("DB**********************************"+response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async insertNewName(FName,LName,Dob,Address,Parent_name,city,phone) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO student_details (FName,LName,Dob,Parent_name,Address,city,phone) VALUES (?,?,?,?,?,?,?);";
+
+                connection.query(query, [FName,LName,Dob,Parent_name,Address,city,phone] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            console.log(insertId); 
+            // return insertId;
+            
+            return {
+                RollNo : insertId,
+                FName:FName,
+                LName:LName,
+                Dob:Dob,
+                Parent_name:Parent_name,
+                Address:Address,
+                city:city,
+                phone:phone
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    // async insertNewName(name) {
+    //     try {
+    //         const dateAdded = new Date();
+    //         const insertId = await new Promise((resolve, reject) => {
+    //             const query = "INSERT INTO names (name, date_added) VALUES (?,?);";
+
+    //             connection.query(query, [name, dateAdded] , (err, result) => {
+    //                 if (err) reject(new Error(err.message));
+    //                 resolve(result.insertId);
+    //             })
+    //         });
+    //         return {
+    //             id : insertId,
+    //             name : name,
+    //             dateAdded : dateAdded
+    //         };
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+}
+
+module.exports=DbService;
